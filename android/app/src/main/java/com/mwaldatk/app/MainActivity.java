99d726c42723b88d22d4159c9a4979f00dadad82
sajjadad589-art <sajjadad589@gmail.com>
2026-08-30 23:26:23 +0300

    Register Android app updater plugin

--- android/app/src/main/java/com/mwaldatk/app/MainActivity.java
+++ android/app/src/main/java/com/mwaldatk/app/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(SunmiPrinterPlugin.class);
+        registerPlugin(AppUpdaterPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
