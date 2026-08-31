1cdbcf0c2cfd56a8d356249f47bebaeb19905f67
sajjadad589-art <sajjadad589@gmail.com>
2026-08-31 19:05:06 +0300

    Handle Android back button inside app navigation

--- android/app/src/main/java/com/mwaldatk/app/MainActivity.java
+++ android/app/src/main/java/com/mwaldatk/app/MainActivity.java
@@ -8,6 +8,21 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(SunmiPrinterPlugin.class);
         registerPlugin(AppUpdaterPlugin.class);
+        registerPlugin(BackNavigationPlugin.class);
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getBridge() != null && getBridge().getWebView() != null) {
+            getBridge().getWebView().post(() ->
+                getBridge().getWebView().evaluateJavascript(
+                    "window.dispatchEvent(new Event('moldatk-android-back'));",
+                    null
+                )
+            );
+            return;
+        }
+        super.onBackPressed();
     }
 }
